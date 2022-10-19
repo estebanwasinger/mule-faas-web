8703a35b37a578854411ae5902fec4f2de1f199b
Esteban Wasinger <estebanwasinger@Estebans-MacBook-Pro.local>
2022-10-19 19:29:51 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import AppInfoView from './views/AppInfoView.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <!-- <RouterView /> -->
+  <h1>Apps</h1>
+
+  <div class="container">
+
+    <AppInfoView name="App Name" status=false />
+    <AppInfoView name="App Name" status=false />
+
+  </div>
+
+</template>
+
+<style scoped>
+.app-info {
+  border: 2px;
+  border-color: black;
+  border-style: dashed;
+}
+
+.container {
+  display: flex;
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/AppInfoView.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    name: string,
+    status: string
+    url?: string
+}>()
+
+</script>
+
+<template>
+
+    <div class="app-info">
+        <div class="left-c">
+            <h2>{{name}}</h2>
+            <p>Status: running</p>
+        </div>
+        <div class="right-c">
+            <button class="child-right">Start</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.app-info {
+    align-content: center;
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+}
+
+.left-c {
+    background-color: lightblue;
+    min-width: 300px;
+    float: left;
+}
+
+.right-c {
+    outline: dashed 1px black;
+    background-color: red;
+    flex-grow: 100;
+    align-items: center;
+  justify-content: center;
+}
+
+.child-right {
+    width: 100 px;
+    /* Center vertically */
+    /* position: absolute; */
+    top: 45%;
+    transform: translate(0, -50%);
+    margin: 0 auto;
+}
+
+
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+}
+
+h3 {
+    font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+    text-align: center;
+}
+
+@media (max-width: 500px) {
+    .app-info {
+        width: 100%;
+    }
+}
+</style>
+
